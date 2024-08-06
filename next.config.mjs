da73060cca8a671d623512657600b0d8d3a400c4
Jia-Wen Li <jiawenli@jia-wens-mbp.home>
2024-08-06 22:27:42 +0100

    Add remotePatterns setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+/*
+ * @Author:Claire Li
+ * @Date:2024-08-06 21:07:07
+ * @LastEditors:Claire Li
+ * @LastEditTime:2024-08-06 22:27:21
+ * @Description:
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// add remotePatterns to use external URL
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.tvmaze.com',
+        port: '',
+        pathname: '/uploads/images/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
