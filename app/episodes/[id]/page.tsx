eebdc492d8dd6c0b6f3d4816ca1520cec4718d1f
Jia-Wen Li <jiawenli@jia-wens-mbp.home>
2024-08-07 19:56:38 +0100

    Add style to show and episode page

--- app/episodes/[id]/page.tsx
+++ app/episodes/[id]/page.tsx
@@ -2,7 +2,7 @@
  * @Author:Claire Li
  * @Date:2024-08-06 22:23:15
  * @LastEditors:Claire Li
- * @LastEditTime:2024-08-07 01:39:49
+ * @LastEditTime:2024-08-07 19:22:10
  * @Description:
  */
 import { fetchEpisodeById } from "@/app/lib/data";
@@ -12,17 +12,26 @@ import Image from 'next/image';
 export default async function Page({ params }: { params: { id: string }}) {
   const episode: Episode = await fetchEpisodeById(Number(params.id))
   return (
-    <main>
-      This is TV show page.
-      <h1>{episode.name}</h1>
-      {episode.summary}
-      <Image 
-        src={episode.image.original}
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Episode image showing desktop version"
-      />
+    <main className="p-10 min-h-screen">
+      <h1 className="font-bold text-3xl mb-5">{episode.name}</h1>
+      <div className="sm:flex">
+        <Image 
+          src={episode.image.medium}
+          width={500}
+          height={380}
+          className="block mb-5 md:hidden"
+          alt="Episode image showing mobile version"
+        />
+        <span dangerouslySetInnerHTML={{__html: episode.summary}} />
+        <Image 
+          src={episode.image.original}
+          width={1000}
+          height={760}
+          className="hidden md:block md:ml-5"
+          alt="Episode image showing desktop version"
+        />
+        
+      </div>
     </main>
   )
 }
--- app/shows/[id]/page.tsx
+++ app/shows/[id]/page.tsx
@@ -2,7 +2,7 @@
  * @Author:Claire Li
  * @Date:2024-08-06 21:10:00
  * @LastEditors:Claire Li
- * @LastEditTime:2024-08-07 01:39:37
+ * @LastEditTime:2024-08-07 19:39:40
  * @Description:
  */
 import { fetchShowById, fetchEpisodesByShowId } from "@/app/lib/data";
@@ -11,46 +11,75 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Page() {
-  const show: Show = await fetchShowById(6771)
-  const episodes: Episode[] = await fetchEpisodesByShowId(6771)
+  const show: Show = await fetchShowById(6771);
+  const episodes: Episode[] = await fetchEpisodesByShowId(6771);
   return (
-    <main>
-      This is TV show page.
-      <h1>{show.name}</h1>
-      {show.summary}
-      <Image 
-        src={show.image.original}
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Show image showing desktop version"
-      />
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Number</th>
-            <th>Date</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        {episodes.map((episode) => {
-          return (
-            <tbody key={episode.id}>
-              <tr>
-                <td>{episode.number}</td>
-                <td>{episode.airdate}</td>
-                <td>
-                  <Link
-                    href={`/episodes/${episode.id}`}
-                  >
-                    {episode.name}
-                  </Link>
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
+    <main className="p-10">
+      <div className="space-y-3">
+        <h1 className="font-bold text-3xl">{show.name}</h1>
+        <div className="sm:flex">
+          <div className="sm:flex">
+            <Image 
+              src={show.image.original}
+              width={250}
+              height={190}
+              className="hidden md:block"
+              alt="Show image showing desktop version"
+            />
+            <Image 
+              src={show.image.medium}
+              width={125}
+              height={95}
+              className="block float-left mr-3 md:hidden"
+              alt="Show image showing mobile version"
+            />
+            <span className="w-full sm:w-2/3 sm:ml-5" dangerouslySetInnerHTML={{__html: show.summary}} />
+          </div>
+          <div className="w-full bg-slate-700 p-5 my-5 rounded-2xl sm:w-1/3 sm:m-5">
+            <h3 className="text-xl">
+              Show Info
+            </h3>
+            <p>
+              Show Type: {show.type}
+            </p>
+            <p className="w-full">
+              Official site: 
+              <a href={show.officialSite ?? undefined} className=" block max-w-full text-blue-500 underline overflow-hidden">
+                {show.officialSite ?? 'N/A'}
+              </a>
+            </p>
+            <p>
+              Rate: {show.rating.average}
+            </p>
+          </div>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Date</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          {episodes.map((episode) => {
+            return (
+              <tbody key={episode.id}>
+                <tr>
+                  <td>{episode.number}</td>
+                  <td>{episode.airdate}</td>
+                  <td>
+                    <Link
+                      href={`/episodes/${episode.id}`}
+                    >
+                      {episode.name}
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
     </main>
   )
 }
